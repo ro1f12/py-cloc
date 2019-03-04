72124268b0e8f9bd545df980a3869588682735ec
Masud Alam Rofi <39493890+ro1f12@users.noreply.github.com>
2019-03-05 00:28:16 +0530

    Final Commit
    Done

---
+++ loc.py
@@ -0,0 +1,86 @@
+#py cloc
+#software enginnering practicatl assignment-1
+#rofi
+#27-02-19
+import os
+import sys
+
+
+
+#function to calculate lines of codes
+def py_cloc(fullpath, dirname, comment_tag, macro_tag):
+    #open the file
+    with open(fullpath) as f:
+        linecount = 0
+        blanklines = 0
+        comments = 0
+        macros = 0
+
+        for line in f:
+            linecount += 1
+
+            nospace = line.strip()
+            if  not nospace:
+                blanklines += 1
+            elif nospace.startswith(comment_tag):
+                comments += 1
+            elif nospace.startswith(macro_tag):
+                macros += 1
+
+
+
+    #diplay the results
+    #print ("-------------------------------------")
+    #print ("\tFile: " + splitall(f_name)[-1] + "\t\t")
+    print ("-------------------------------------")
+    print ("Lines          : " + str(linecount))
+    print ("Blank lines:   : " + str(blanklines))
+    print ("Macro lines    : " + str(macros))
+    print ("Comment lines  : " + str(comments))
+    print ("-------------------------------------")
+    print ("Lines of code  :" + str(linecount - blanklines - macros - comments))
+    print ("-------------------------------------")
+
+
+#main function
+def main():
+    if len(sys.argv) != 2:
+        print ("\t\tUsage: <program> <C file name>")
+        quit()
+
+    #file name from commandline arg
+    f_name = sys.argv[1]
+    if not f_name:
+        print ("\t\tUsage: <program> <C file name>")
+        quit()
+
+    #present working directory
+    dirname = os.getcwd()
+
+    #set the full path
+    fullpath = os.path.join(dirname, f_name)
+
+    #check the file extension
+    extension = os.path.splitext(fullpath)[1]
+
+    #tags for counting macros i.e. #include<...> of #define and comments
+    comment_tag = "//"
+    macro_tag = "#"
+
+    #check if the file exists and C-file or not
+    if not extension == ".c" or not os.path.exists(fullpath):
+        #print("\t\tError: provide a C file or File does not exits")
+        print("\t\tUsage: <Program> <C- file name>")
+        quit()
+    #function call for calculating lines of code
+    py_cloc(fullpath, dirname, comment_tag, macro_tag)
+
+#driver code
+if __name__ == "__main__":
+
+    print("******************************************************************\t")
+    print("\t\tLINES OF CODE COUNTER IN PYTHON\t\t")
+    print("\t\t\t\tby Rofi\t\t")
+    print("******************************************************************\t")
+
+    main()
